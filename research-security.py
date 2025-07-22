a6c7f7323ad5b6c4c260ce15420c5e5f4ba9b01c
Richard Campo <rjgc.richard@gmail.com>
2025-07-21 20:56:17 -0400

    make simple risk calculator

--- research-security.py
+++ research-security.py
@@ -1,6 +1,31 @@
 import requests
 import pandas as pd
 
+def risk_calc(paper):
+    score = 0
+    scored_countries = {'cn': 2, 'ru': 2, 'ir': 3, 'kp': 5}
+    keywords = ['defense', 'defence', 'surveillance', 'military', 'army'
+                'navy', 'air force', 'artificial intelligence', 'quantum',
+                'semiconductor', 'microelectronic']
+
+    for author in paper.get('authorships', []):
+        inst = author.get('institution', {})
+        country = inst.get('country_code')
+        name = inst.get("display_name", "").lower()
+        
+        if country in scored_countries:
+            score += scored_countries[country]
+
+        for k in keywords:
+            if k in name:
+                score += 5
+        
+        if not author.get('author', {}).get('orcid'):
+            score += 1
+
+    return score
+
+
 BASE_URL = 'https://api.openalex.org/works'
 
 # Parameters
