f32c2706fa0e71512ad62c399a411ab109920f5f
Richard Campo <rjgc.richard@gmail.com>
2025-07-21 20:41:12 -0400

    get 2024 papers

--- research-security.py
+++ research-security.py
@@ -5,15 +5,14 @@ BASE_URL = 'https://api.openalex.org/works'
 
 # Parameters
 params = {
+    'filters': 'publication_year:2024',
     'mailto': 'rjgc.richard@gmail.com',
     'per_page': 25
 }
 
 # Request
 response = requests.get(BASE_URL, params=params)
-json = response.json()
-
-works = json['results']
+works = response.json()['results']
 
 data = pd.DataFrame(works)
 
